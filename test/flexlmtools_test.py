8bb75b0571633ebe4655e7fa9dba52be306e0180
Sho Hirose <sho.hirose@gmail.com>
2021-07-31 00:08:55 +0900

    Create tests

---
+++ test/flexlmtools_test.py
@@ -0,0 +1,37 @@
+from flexlmtools import parse_query, validate_server_name
+import pytest
+
+APP_FEATURES = {'feat1': (2, 0), 'feat2': (5, 1),
+                'feat6': (3, 3), 'feat-add': (2, 0),
+                'feat_opt': (1, 1)}
+
+
+def test_parse_query():
+    with open('./test/app-features.txt', 'r') as f:
+        lines = f.read()
+
+    def check(feature):
+        dct = parse_query(lines, features=[feature])
+        assert feature in dct
+        assert dct[feature] == APP_FEATURES[feature]
+
+    check('feat1')
+    check('feat2')
+    check('feat6')
+    check('feat-add')
+    check('feat_opt')
+
+    dct = parse_query(lines, features=['feat3'])
+    # Check if dct is empty
+    assert not dct
+
+    dct = parse_query(lines)
+    assert all(feature in dct for feature in APP_FEATURES)
+    assert all(dct[feature] == values for feature,
+               values in APP_FEATURES.items())
+
+def test_validate_server_name():
+    assert validate_server_name('6200@test-server.org')
+    assert not validate_server_name('0123@test-serv')
+    assert not validate_server_name('1234@test_serv')
+    assert not validate_server_name('3100@test#serv')
